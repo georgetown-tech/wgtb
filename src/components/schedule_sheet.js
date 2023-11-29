5966023443abd0a3453c8e2e8454cd703fb36268
kjpalmer25 <kjpalmer24@outlook.com>
2023-11-28 19:49:12 -0500

    merging sunils changes

---
+++ src/components/schedule_sheet.js
@@ -0,0 +1,23 @@
+// src/components/GoogleSheetEmbed.js
+import React from "react";
+import "./googlesheet.css"; // Import a separate CSS file for styling
+
+const GoogleSheetEmbed = () => {
+  return (
+    <div className="google-sheet-container">
+      <iframe
+        title="Google Sheet Embed"
+        className="google-sheet-iframe"
+        src="https://docs.google.com/spreadsheets/d/1UU-dhZu0D0RkqyRYkIzQMVXrjiE8UfNwBCJc5xevrGM/edit?rm=minimal"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default GoogleSheetEmbed;
+
+
+
+
